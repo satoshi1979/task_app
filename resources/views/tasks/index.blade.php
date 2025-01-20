722f7759c7746610b1323b6a1fffb1ad8045c13e
satoshi1979 <satoshi19791212@gmail.com>
2025-01-20 20:54:53 +0900

    機能をすべて作成。最終チェックの手前

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>task index</title>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
@@ -13,8 +14,13 @@
 
 
     @foreach ($tasks as $task)
-        <!-- // リンク先をidで取得し名前で出力 -->
-        <a href="/tasks/{{ $task->id }}">{{ $task->title }}</a><br>
+        <div class="button-group">
+        <a href="/tasks/{{ $task->id }}">{{ $task->title }}</a><form action="{{ route('tasks.destroy', $task) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button onclick="if(!confirm('削除しますか？')){return false};">削除する</button>
+        </form>
+        </div>
     @endforeach
 
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -17,15 +17,12 @@
     <p>{{ $task->body }}</p>
     <div class="button-group">
         <button onclick="location.href='{{ route('tasks.index') }}'">一覧へ戻る</button>
-        <button onclick="location.href='{{ route('tasks.edit', $task) }}'">編集する</button>
-        {{-- <input type="button" onclick="location.href='/tasks'" value="削除する"> --}}
 
 
-        <!-- $memoのidを元に編集ページへ遷移する -->
         <form action="{{ route('tasks.destroy', $task) }}" method="post">
             @csrf
             @method('DELETE')
-            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+            <button onclick="if(!confirm('削除しますか？')){return false};">削除する</button>
         </form>
     </div>
 
